9d897cfb2e1a19ca71d1843521de5a4ecebe2482
Aleksandr Chigarev <a.chigarev96@gmail.com>
2022-07-06 06:42:23 +0500

    Интерполяция + слушатель события

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
-    
+    <div>
+        <button @click="addLike">Like</button>
+        <button @click="addDislike">Dislike</button>
+    </div>
+    <div>
+        Количества лайков : <strong>{{likes}}</strong>
+    </div>
+    <div>
+        Количества дизлайков : <strong>{{dislikes}}</strong> 
+    </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            likes: 0,
+            dislikes: 5,
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes += 1;
+        },
+        addDislike() {
+            this.dislikes -= 1;
+        }
+    }
 }
 </script>
 
