921477757517356a5261e64c9e137b056e4771b5
Aleksandr Chigarev <a.chigarev96@gmail.com>
2022-07-06 22:19:28 +0500

    v-model

--- src/components/UI/MyInput.vue
+++ src/components/UI/MyInput.vue
@@ -1,10 +1,18 @@
 <template>
-    <input class="input" type="text">
+    <input :value="modelValue" @input="updateValue" class="input" type="text">
 </template>
 
 <script>
 export default {
     name: "my-input",
+    props: {
+        modelValue: [String, Number],
+    },
+    methods: {
+        updateValue(event){
+            this.$emit('update:modelValue', event.target.value);
+        }
+    }
 }
 </script>
 
