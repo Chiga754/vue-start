22f261f240e8571c9305822ed8be2b18878a3270
Aleksandr Chigarev <a.chigarev96@gmail.com>
2022-07-07 23:29:46 +0500

    Пагинация

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,16 @@
         </my-dialog>
         <post-list :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isPostsLoading"/>
         <h1 v-else>Идет загрузка...</h1>
+        <div class="page__wrapper">
+            <div 
+                v-for="pageNumber in totalPage" 
+                :key="pageNumber" 
+                class="page"
+                :class="{'current-page': page === pageNumber}"
+                @click="changePage(pageNumber)"
+                >{{ pageNumber }}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -37,6 +47,9 @@ export default {
             isPostsLoading: false,
             selectedSort: '',
             searchQuery: '',
+            page: 1,
+            limit: 10,
+            totalPage: 0,
             sortOptions: [
                 {value: 'title', name: "По названию"},
                 {value: 'body', name: "По описанию"},
@@ -54,10 +67,19 @@ export default {
         showDialog() {
             this.dialogVisible = true;
         },
+        changePage(pageNumber) {
+            this.page = pageNumber;
+        },
         async fetchPosts() {
             try {
                 this.isPostsLoading = true;
-                const respons = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+                const respons = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                    params: {
+                        _page: this.page,
+                        _limit: this.limit,
+                    }
+                });
+                this.totalPage = Math.ceil(respons.headers['x-total-count'] / this.limit);
                 this.posts = respons.data;
             } catch (error) {
                 alert('Ошибка')
@@ -80,13 +102,16 @@ export default {
         }
     },
 
-    // watch: {
-    //     selectedSort(newValue) {
-    //         this.posts.sort((post1, post2) => {
-    //             return post1[newValue]?.localeCompare(post2[newValue]);
-    //         })
-    //     }
-    // }
+    watch: {
+        page() {
+            this.fetchPosts();
+        }
+        // selectedSort(newValue) {
+        //     this.posts.sort((post1, post2) => {
+        //         return post1[newValue]?.localeCompare(post2[newValue]);
+        //     })
+        // }
+    }
 }
 </script>
 
@@ -103,5 +128,17 @@ export default {
         margin: 15px 0;
         display: flex;
         justify-content: space-between;
+    }
+    .page__wrapper{
+        display: flex;
+        margin-top: 15px;
+    }
+    .page{
+        border: 1px solid black;
+        padding:  10px;
+        cursor: pointer;
+    }
+    .current-page{
+        border: 2px solid teal;
     }
 </style>
--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="post">
         <div>
-            <strong>Название</strong> : {{post.title}}
-            <strong>Описание</strong> : {{post.body}}
+            <div>{{ post.id }}</div>
+            <div><strong>Название</strong> : {{post.title}}</div>
+            <div><strong>Описание</strong> : {{post.body}}</div>
         </div>
         <div class="post__btns">
             <my-button 
